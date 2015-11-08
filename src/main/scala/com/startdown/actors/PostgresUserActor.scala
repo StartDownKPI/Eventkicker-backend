bc9fc6f4b0d0097060408ea52a38bf30264c51ef
infm <syniuhin@gmail.com>
2015-11-08 15:25:54 +0200

    Added generalization for an Actor

--- src/main/scala/com/startdown/actors/PostgresUserActor.scala
+++ src/main/scala/com/startdown/actors/PostgresUserActor.scala
@@ -6,7 +6,7 @@ import com.startdown.models.{User, UserDao}
 import com.startdown.utils.CRUD
 import spray.json._
 
-import scala.concurrent.Promise
+import scala.concurrent.Future
 
 /**
   * infm created it with love on 11/7/15. Enjoy ;)
@@ -15,26 +15,51 @@ import scala.concurrent.Promise
 object PostgresUserActor extends CRUD[User, String] {}
 
 class PostgresUserActor extends Actor {
+
   import PostgresUserActor._
   import com.startdown.models.UserJsonProtocol._
   import context.dispatcher
 
+  case class Response(success: Boolean,
+                      single: Option[User] = None,
+                      multiple: Option[Seq[User]] = None,
+                      message: Option[String] = None)
+
+  implicit val responseFormat = jsonFormat4(Response)
+
+  def makeResponse(f: Future[Any]) =
+    f.map {
+      case single: Some[User] => new Response(true, single = single)
+      case multiple: Seq[User] => new Response(true, multiple = Some(multiple))
+      case None => new Response(false)
+      case _ => new Response(true)
+    }.recover { case cause => new Response(false, message = Some(cause
+      .toString)) }
+      .map { case r => r.toJson.compactPrint }
+
   override def receive = {
     case FetchAll =>
-      UserDao.listAllUsers.map(_.toJson.compactPrint) pipeTo sender
+      makeResponse(UserDao.listAllUsers) pipeTo sender
+
     case Create(u: User) =>
-      UserDao.addUser(u).map(_.toJson.compactPrint) pipeTo sender
+      makeResponse(UserDao.addUser(u)) pipeTo sender
+
     case Read(username: String) =>
-      UserDao.findUser(username).map(_.toJson.compactPrint) pipeTo sender
+      makeResponse(UserDao.findUser(username)) pipeTo sender
+
     case Update(u: User) =>
-      UserDao.updateUser(u).map(_.toJson.compactPrint) pipeTo sender
+      makeResponse(UserDao.updateUser(u)) pipeTo sender
+
     case Delete(username: String) =>
-      UserDao.deleteUser(username).map(_.toJson.compactPrint) pipeTo sender
+      makeResponse(UserDao.deleteUser(username)) pipeTo sender
+
     case DeleteAll =>
-      UserDao.deleteAll.map(_.toJson.compactPrint) pipeTo sender
+      makeResponse(UserDao.deleteAll) pipeTo sender
+
     case CreateTable =>
-      UserDao.createTable.map(_.toJson.compactPrint) pipeTo sender
+      makeResponse(UserDao.createTable.map(_.toJson.compactPrint)) pipeTo sender
+
     case DropTable =>
-      UserDao.dropTable.map(_.toJson.compactPrint) pipeTo sender
+      makeResponse(UserDao.dropTable.map(_.toJson.compactPrint)) pipeTo sender
   }
 }
